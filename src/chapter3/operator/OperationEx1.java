ba950adf43d98ac3abd7f14a2ca4b6c70d2cd57c
japark <japark720@gmail.com>
2024-03-20 13:01:10 +0900

    Test : 비트연산자 테스트 코드 작성 	- 비트연산자 예제

--- src/chapter3/operator/OperationEx1.java
+++ src/chapter3/operator/OperationEx1.java
@@ -3,8 +3,11 @@ package chapter3.operator;
 public class OperationEx1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int mathScore = 90;
+		int engScore = 75;
+		
+		int totalScore = mathScore + engScore;
+		System.out.println(totalScore);
 	}
 
 }
---
+++ src/chapter3/operator/OperationEx5.java
@@ -0,0 +1,20 @@
+package chapter3.operator;
+
+// 비트 이동연산자 : <<, >>, >>>
+//https://coding-factory.tistory.com/521
+
+public class OperationEx5 {
+	public static void main(String[] args) {
+		int num = 0B10000101;	// 2진수 8bit
+		
+		System.out.println(num << 2);
+		System.out.println(num >> 2);
+		
+		System.out.println(num >>> 2);
+		System.out.println(num);
+		
+		num = num << 2;
+		System.out.println(num);
+		
+	}
+}
