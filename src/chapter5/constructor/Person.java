4dff8f018a2cf99760585de4b2ffb14536cd7302
japark <japark720@gmail.com>
2024-03-21 17:42:12 +0900

    Test : 커밋테스트
    - 커밋테스트

--- src/chapter5/constructor/Person.java
+++ src/chapter5/constructor/Person.java
@@ -1,5 +1,10 @@
 package chapter5.constructor;
 
+// 생성자 예제 (Constructor) 
 public class Person {
-
+	
+	String name;
+	float height;
+	float weight;
+	
 }
--- src/chapter5/constructor/PersonTest.java
+++ src/chapter5/constructor/PersonTest.java
@@ -1,5 +1,10 @@
 package chapter5.constructor;
 
 public class PersonTest {
-
+	public static void main(String[] args) {
+		
+		Person p1 = new Person();
+		p1.height = 170f;
+		
+	}
 }
